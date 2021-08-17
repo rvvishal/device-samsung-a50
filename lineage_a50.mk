749c73b40323e3857aeda69ba0480a40a458310a
Vishal Rajbhar <71400384+rvvishal@users.noreply.github.com>
2021-08-17 14:50:56 +0530

    A50 : Added FOD (Inscreen Fingerprint)

--- lineage_a50.mk
+++ lineage_a50.mk
@@ -3,4 +3,7 @@ $(call inherit-product, device/samsung/a50/full_a50.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+## Inscreen Fingerprint HAL
+TARGET_HAVE_FOD := true
+
 PRODUCT_NAME := lineage_a50

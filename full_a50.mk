840af94f863825dc9a4ffffb1846fded77c5e962
Vishal Rajbhar <71400384+rvvishal@users.noreply.github.com>
2021-08-17 14:48:35 +0530

    A50 : oreo to pie

--- full_a50.mk
+++ full_a50.mk
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p_mr1.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
